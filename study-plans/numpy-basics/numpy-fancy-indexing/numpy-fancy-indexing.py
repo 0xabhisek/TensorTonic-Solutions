f74cf16f33639cff4adb2291f27b73ca0b5fbd57
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-04-27 21:12:12 +0530

    Add solution for numpy-fancy-indexing

---
+++ study-plans/numpy-basics/numpy-fancy-indexing/numpy-fancy-indexing.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def select_by_index(arr, indices, axis):
+    """
+    Returns: 2D ndarray of float64
+    """
+    a = np.array(arr, dtype = np.float64)
+    idx = np.array(indices)
+
+    if axis == 0:
+        return a[idx]
+    return a[:,idx]
