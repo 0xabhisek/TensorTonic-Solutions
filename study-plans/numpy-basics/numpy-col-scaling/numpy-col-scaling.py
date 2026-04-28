e823fa3e33ca95787dde5a42f04331353e387109
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-04-28 17:36:35 +0530

    Update solution for numpy-col-scaling

--- study-plans/numpy-basics/numpy-col-scaling/numpy-col-scaling.py
+++ study-plans/numpy-basics/numpy-col-scaling/numpy-col-scaling.py
@@ -4,4 +4,4 @@ def scale_cols(data, weights):
     """Returns: np.ndarray of shape (m, n), each column scaled by corresponding weight"""
     a = np.array(data, dtype = np.float64)
     b = np.array(weights, dtype = np.float64)
-    return a*b[np.newaxis,:]
+    return a*b
