819dd2d901bcb5f0e617bc0dfdc7dae79fa47cb9
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-05-18 15:55:05 +0530

    Add solution for probstat-sample-var-std

---
+++ study-plans/math-probability/probstat-sample-var-std/probstat-sample-var-std.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def sample_var_std(x):
+    """
+    Returns: dict with 'variance' and 'std_dev' as floats.
+    """
+    x = np.asarray(x, dtype =  float)
+    n = len(x)
+    mean = np.mean(x)
+    var = float(np.sum((x - mean) ** 2)/(n-1))
+    std = float(np.sqrt(var))
+    return {'variance': var, 'std_dev': std}
