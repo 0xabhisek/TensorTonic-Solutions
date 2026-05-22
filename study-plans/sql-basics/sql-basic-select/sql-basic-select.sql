497e064f10b0df1b7e92c72f4fa6db3e84480cbd
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-05-22 21:00:54 +0530

    Update solution for sql-basic-select

--- study-plans/sql-basics/sql-basic-select/sql-basic-select.sql
+++ study-plans/sql-basics/sql-basic-select/sql-basic-select.sql
@@ -1,3 +1,6 @@
 -- Write your SQL query here
-select product_name as name, category, (unit_price * units_in_stock) as inventory_value
-from products
+SELECT
+    product_name AS name,
+    category,
+    unit_price * units_in_stock AS inventory_value
+FROM products;
