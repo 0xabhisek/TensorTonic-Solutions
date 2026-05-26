a469d79d3549268832dace2f6f652997dfd3b239
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-05-26 10:48:07 +0530

    Update solution for la-matrix-vector-multiply

--- study-plans/math-linear-algebra/la-matrix-vector-multiply/la-matrix-vector-multiply.py
+++ study-plans/math-linear-algebra/la-matrix-vector-multiply/la-matrix-vector-multiply.py
@@ -6,10 +6,5 @@ def matrix_vector_multiply(A, x):
     """
     x = np.asarray(x, dtype = np.float64)
     A = np.asarray(A, dtype = np.float64)
-    m = len(A)
-    y = np.zeros((m))
-    for i in range(m):
-        y[i] = A[i]@x
-
-    return y
+    return A@x
         
