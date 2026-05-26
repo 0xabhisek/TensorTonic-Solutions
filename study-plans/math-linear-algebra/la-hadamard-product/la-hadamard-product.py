4d189cd28fba1cf776cb69e97eefc9dd01bf9e5a
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-05-26 10:26:50 +0530

    Update solution for la-hadamard-product

--- study-plans/math-linear-algebra/la-hadamard-product/la-hadamard-product.py
+++ study-plans/math-linear-algebra/la-hadamard-product/la-hadamard-product.py
@@ -6,12 +6,5 @@ def hadamard_product(A, B):
     """
     A = np.asarray(A)
     B = np.asarray(B)
-    m = len(A)
-    n = len(A[0])
-    x = np.zeros((m,n))
-    for i in range(m):
-        for j in range(n):
-            x[i][j] = A[i][j] * B[i][j]
-
-    return x
+    return A * B
             
