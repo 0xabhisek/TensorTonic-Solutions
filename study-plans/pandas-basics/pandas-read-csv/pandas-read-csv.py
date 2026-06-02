709a56bab268312e324195e2e168798cac6694c6
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-02 21:41:07 +0530

    Update solution for pandas-read-csv

--- study-plans/pandas-basics/pandas-read-csv/pandas-read-csv.py
+++ study-plans/pandas-basics/pandas-read-csv/pandas-read-csv.py
@@ -6,7 +6,8 @@ def create_dataframe(data):
     """
     df = pd.DataFrame(data)
     return {
-        'data' : df.to_dict('list'),
+        'data': df.to_dict('list'),
         'shape': list(df.shape),
         'columns': df.columns.tolist()
     }
+ 
