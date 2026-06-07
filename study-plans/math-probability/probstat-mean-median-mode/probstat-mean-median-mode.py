f5f9b2ff77d54bd743be809c680c591484d9f5d7
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-07 20:47:11 +0530

    Solve probstat-mean-median-mode

--- study-plans/math-probability/probstat-mean-median-mode/probstat-mean-median-mode.py
+++ study-plans/math-probability/probstat-mean-median-mode/probstat-mean-median-mode.py
@@ -1,13 +1,14 @@
 import numpy as np
 from collections import Counter
+from scipy import stats
 
 def mean_median_mode(x):
     """
     Returns: dict with 'mean', 'median', 'mode' as floats.
     """
-    x = np.array(x, dtype = float)
-    m = float(np.mean(x))
-    md = float(np.median(x))
-    cnt = Counter(x)
-    mo = float(max(cnt, key = cnt.get))
-    return {"mean" : m, 'median' : md, 'mode' : mo}
+    x = np.array(x,dtype = float)
+    mean = np.mean(x)
+    median = np.median(x)
+    mode = stats.mode(x).mode
+    return {'mean': mean, 'median': median, 'mode': mode}
+    
