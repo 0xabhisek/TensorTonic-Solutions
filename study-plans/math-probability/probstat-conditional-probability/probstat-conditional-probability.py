fc1bd31b22abf30dd483dae567ef731693f13516
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-08 12:54:16 +0530

    Solve probstat-conditional-probability

--- study-plans/math-probability/probstat-conditional-probability/probstat-conditional-probability.py
+++ study-plans/math-probability/probstat-conditional-probability/probstat-conditional-probability.py
@@ -2,7 +2,7 @@ def conditional_probability(p_a, p_b, p_a_and_b):
     """
     Returns: [p_a_given_b, p_b_given_a] as a list.
     """
-    pa_b = round(p_a_and_b/p_b, 4)
-    pb_a = round(p_a_and_b/p_a, 4)
-
-    return [pa_b, pb_a]
+    
+    p_a_given_b = round(p_a_and_b / p_b, 4)
+    p_b_given_a = round(p_a_and_b / p_a, 4)
+    return [p_a_given_b, p_b_given_a]
