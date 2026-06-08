7247457a203a06b16ad801ad7d15396e41101234
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-08 13:14:56 +0530

    Solve probstat-permutations-combinations

--- study-plans/math-probability/probstat-permutations-combinations/probstat-permutations-combinations.py
+++ study-plans/math-probability/probstat-permutations-combinations/probstat-permutations-combinations.py
@@ -4,4 +4,4 @@ def perms_and_combs(n, r):
     """
     Returns: [permutations, combinations, factorial] as a list.
     """
-    return [perm(n,r), comb(n,r), factorial(n)]
+    return [math.perm(n,r), math.comb(n,r),math.factorial(n)]
