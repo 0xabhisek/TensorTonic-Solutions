37f00f637a5545791b27196c45dc8add237f74a9
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-08 14:40:10 +0530

    Solve probstat-poisson-distribution

--- study-plans/math-probability/probstat-poisson-distribution/probstat-poisson-distribution.py
+++ study-plans/math-probability/probstat-poisson-distribution/probstat-poisson-distribution.py
@@ -4,6 +4,11 @@ def poisson_distribution(lam, max_k):
     """
     Returns: [pmf_list, cdf_at_max_k, p_zero] as a list.
     """
-    pmf = [round((lam**k)* (exp(-lam))/factorial(k), 4 ) for k in range(max_k + 1)]
-    cdf = round(sum(pmf),4)
-    return [pmf, cdf, pmf[0]]
+    n = max_k
+    pmf = [round((lam**k * exp(-lam))/factorial(k),4) for k in range(n+1)]
+    cdf = round(sum(pmf[:max_k + 1]),4)
+    p0 = round(pmf[0],4)
+
+    return[pmf,cdf,p0]
+
+    
