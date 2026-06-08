2646f6fd19693b7f61253a515849b7686c6ee6c9
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-08 19:03:26 +0530

    Solve probstat-exponential-distribution

--- study-plans/math-probability/probstat-exponential-distribution/probstat-exponential-distribution.py
+++ study-plans/math-probability/probstat-exponential-distribution/probstat-exponential-distribution.py
@@ -4,10 +4,10 @@ def exponential_distribution(lam, t):
     """
     Returns: dict with 'pdf', 'cdf', 'survival', 'mean', 'variance' as floats.
     """
-    rep = lambda x : float(round(x,4))
-    pdf = rep(lam * exp(-lam * t))
-    cdf = rep(1 - exp(-lam * t) )
-    sf = rep(exp(-lam * t))
-    m = rep(1/lam)
-    v = rep(1/(lam**2))
-    return {"pdf": pdf, "cdf": cdf, "survival": sf, "mean": m, "variance": v}
+    pdf = round(lam*(exp(-lam*t)),4)
+    cdf = round(1 - exp(-lam*t),4)
+    survival = round(exp(-lam*t),4)
+    mean = round(1/lam,4)
+    variance = round(1/lam**2,4)
+
+    return {'pdf': pdf, 'cdf': cdf, 'survival' : survival, 'mean': mean, 'variance': variance }
