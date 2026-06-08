4576b3651dddbc537d78217c0349fb4f1c9bc420
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-08 13:10:37 +0530

    Solve probstat-independence-testing

--- study-plans/math-probability/probstat-independence-testing/probstat-independence-testing.py
+++ study-plans/math-probability/probstat-independence-testing/probstat-independence-testing.py
@@ -2,6 +2,9 @@ def independence_test(p_a, p_b, p_a_and_b):
     """
     Returns: dict with 'p_a_times_p_b' (float) and 'is_independent' (bool).
     """
+    
     p_a_times_p_b = round(p_a * p_b,4)
-    is_independent = (p_a_times_p_b == p_a_and_b)
-    return {'p_a_times_p_b': p_a_times_p_b, 'is_independent': is_independent}
+
+    is_independent = (p_a_times_p_b == round(p_a_and_b,4))
+
+    return { 'p_a_times_p_b' : round(p_a_times_p_b,4), 'is_independent': is_independent }
