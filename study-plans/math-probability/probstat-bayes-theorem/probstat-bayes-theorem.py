b5fd54806ed6f0515235331c538b04fccb925ab2
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-08 13:05:17 +0530

    Solve probstat-bayes-theorem

--- study-plans/math-probability/probstat-bayes-theorem/probstat-bayes-theorem.py
+++ study-plans/math-probability/probstat-bayes-theorem/probstat-bayes-theorem.py
@@ -2,6 +2,5 @@ def bayes_theorem(p_a, p_b_given_a, p_b_given_not_a):
     """
     Returns: float, the posterior probability P(A|B).
     """
-    p_b = round(float(p_a*p_b_given_a + (1-p_a)*p_b_given_not_a) , 4)
-    pa_b = round(float(p_b_given_a*p_a/p_b),4)
+    pa_b = round((p_b_given_a*p_a)/(p_b_given_a*p_a + p_b_given_not_a*(1 - p_a))  ,4)
     return pa_b
