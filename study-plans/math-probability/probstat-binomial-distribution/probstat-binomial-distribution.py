07d574d4728312eb71747c9a50bcc13aa94e9ba7
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-08 14:25:30 +0530

    Solve probstat-binomial-distribution

--- study-plans/math-probability/probstat-binomial-distribution/probstat-binomial-distribution.py
+++ study-plans/math-probability/probstat-binomial-distribution/probstat-binomial-distribution.py
@@ -2,11 +2,25 @@ from math import comb
 
 def binomial_distribution(n, p, threshold):
     """
-    Returns: dict with 'pmf' (list), 'mean', 'variance', 'tail_prob' as floats.
+    Returns: dict with 'pmf' (list), 'mean', 'variance', 'prob_at_least'
     """
-    mean = round(n * p , 4)
-    var = round(n * p * (1-p), 4)
-    pmf = [round(comb(n,k) * (p ** k) * ((1-p) ** (n-k)),4) for k in range(n+1)]
-    pal = round(sum(pmf[threshold:]), 4)
 
-    return {'pmf': pmf, 'mean': mean, 'variance': var, 'prob_at_least': pal}
+    pmfr = [
+        comb(n, k) * (p**k) * ((1-p)**(n-k))
+        for k in range(n+1)
+    ]
+
+    pmf = [round(x, 4) for x in pmfr]
+
+    m = round(n * p, 4)
+
+    var = round(n * p * (1-p), 4)
+
+    tp = round(sum(pmf[threshold:]), 4)
+
+    return {
+        'pmf': pmf,
+        'mean': m,
+        'variance': var,
+        'prob_at_least': tp
+    }
