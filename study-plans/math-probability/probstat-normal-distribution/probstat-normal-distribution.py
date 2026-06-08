0e0e05287702c75598926652ee3ec051cbf122f9
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-08 18:06:11 +0530

    Solve probstat-normal-distribution

--- study-plans/math-probability/probstat-normal-distribution/probstat-normal-distribution.py
+++ study-plans/math-probability/probstat-normal-distribution/probstat-normal-distribution.py
@@ -1,11 +1,12 @@
 from scipy import stats
+import numpy as np
 
 def normal_distribution(mu, sigma, x):
     """
     Returns: dict with 'z_score', 'cdf', 'pdf', 'prob_within_1std' as floats.
     """
-    z = round((x - mu)/sigma, 4)
-    pdf = round(float(stats.norm.pdf(x,mu,sigma)),4)
-    cdf = round(float(stats.norm.cdf(x,mu,sigma)), 4)
-    p1 = round(  float(stats.norm.cdf(mu + sigma ,mu,sigma)) - float(stats.norm.cdf(mu - sigma,mu,sigma)) , 4)
-    return {"z_score" : z, "cdf" : cdf, "pdf": pdf, "prob_within_1_std" : p1}
+    z_score = round((x - mu)/sigma,4)
+    cdf = round(stats.norm.cdf(x,mu,sigma),4)
+    pdf = round(stats.norm.pdf(x,mu,sigma),4)
+    p1 = round(stats.norm.cdf(sigma+mu,mu,sigma) - stats.norm.cdf(mu-sigma,mu,sigma),4)
+    return {"z_score": z_score, "cdf": cdf, "pdf": pdf, "prob_within_1_std": round(p1,4)}
