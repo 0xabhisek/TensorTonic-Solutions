aca82d7e2225da40af130239d578d8f8ef46810b
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-09 12:03:53 +0530

    Solve probstat-bayesian-updating

--- study-plans/math-probability/probstat-bayesian-updating/probstat-bayesian-updating.py
+++ study-plans/math-probability/probstat-bayesian-updating/probstat-bayesian-updating.py
@@ -1,12 +1,9 @@
 def bayesian_update(prior_alpha, prior_beta, successes, failures):
-    """
-    Returns: dict with 'posterior_alpha', 'posterior_beta', 'prior_mean', 'posterior_mean' as floats.
-    """
-    posterior_alpha = round(prior_alpha + successes, 4)
-    posterior_beta = round(prior_beta + failures,4)
+    """Returns: dict with 'posterior_alpha', 'posterior_beta', 'prior_mean', 'posterior_mean' as floats."""
+    post_a = prior_alpha + successes
+    post_b = prior_beta + failures
+    prior_mean = round(prior_alpha / (prior_alpha + prior_beta), 4)
+    post_mean = round(post_a / (post_a + post_b), 4)
+    return {"posterior_alpha": post_a, "posterior_beta": post_b, "prior_mean": prior_mean, "posterior_mean": post_mean}
 
-    prior_mean = round(prior_alpha /(prior_alpha + prior_beta),4)
-    posterior_mean = round(posterior_alpha/(posterior_alpha + posterior_beta),4)
-
-    return {"posterior_alpha": posterior_alpha, "posterior_beta": posterior_beta, "prior_mean": prior_mean, "posterior_mean": posterior_mean}
     
