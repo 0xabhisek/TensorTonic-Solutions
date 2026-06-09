0d913227418e4d992b51ed18be4b95800859782d
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-09 11:58:48 +0530

    Solve probstat-mle-normal

--- study-plans/math-probability/probstat-mle-normal/probstat-mle-normal.py
+++ study-plans/math-probability/probstat-mle-normal/probstat-mle-normal.py
@@ -5,7 +5,8 @@ def mle_normal(data):
     Returns: dict with 'mu_hat' and 'sigma_hat' as floats (MLE estimates).
     """
     x = np.array(data, dtype = float)
-    mu_hat = round(np.mean(x),4)
+    n = len(x)
+    mu = round(float((1/n)*(np.sum(x))),4)
+    sig = round(float((1/n)*(np.sum((x - mu)**2)))**0.5,4)
 
-    sigma_hat = round((np.mean((x - mu_hat)**2))**0.5 , 4 )
-    return { "mu_mle": mu_hat, "sigma_mle": sigma_hat}
+    return {"mu_mle": mu, "sigma_mle": sig }
