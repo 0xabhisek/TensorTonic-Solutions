8138e2e9d0279153d5e2fbe253f47eba54299ef3
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-09 09:32:33 +0530

    Solve probstat-z-test-one-sample

--- study-plans/math-probability/probstat-z-test-one-sample/probstat-z-test-one-sample.py
+++ study-plans/math-probability/probstat-z-test-one-sample/probstat-z-test-one-sample.py
@@ -4,6 +4,7 @@ def z_test_one_sample(x_bar, mu_0, sigma, n, alpha):
     """
     Returns: [z_stat, p_value, reject] as a list.
     """
-    z = round((x_bar - mu_0) / (sigma / n**0.5), 4)
-    p_val = round(2 * float(stats.norm.sf(abs(z))), 4)
-    return [z, p_val, p_val < alpha]
+    r = lambda x: round(x,4)    
+    z_stat = r((x_bar - mu_0)/(sigma/(n**0.5)))
+    p = r(2 * stats.norm.cdf(-abs(z_stat)))
+    return [z_stat,p,p<alpha]
