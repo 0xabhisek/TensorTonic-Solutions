592cf56185c9396cdf61b687664c48209ddbb9af
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-10 07:54:50 +0530

    Solve percentiles

--- percentiles/percentiles.py
+++ percentiles/percentiles.py
@@ -5,5 +5,5 @@ def percentiles(x, q):
     Compute percentiles using linear interpolation.
     """
     x =  np.array(x)
-    p = np.percentile(x,q)
+    p = np.percentile(x,q, method = 'linear')
     return p
