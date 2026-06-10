34c0733549516e853462578beb8062a5738f8d2b
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-10 11:13:06 +0530

    Solve pandas-handle-missing

--- study-plans/pandas-basics/pandas-handle-missing/pandas-handle-missing.py
+++ study-plans/pandas-basics/pandas-handle-missing/pandas-handle-missing.py
@@ -5,10 +5,9 @@ def handle_missing(data, fill_value):
     Returns: dict with 'null_counts' (dict) and 'cleaned_data' (dict)
     """
     df = pd.DataFrame(data)
-    nc = {k: int(v) for k,v in df.isnull().sum().items()}
-    df = df.fillna(fill_value)
-
+    nc = df.isnull().sum().to_dict()
+    f = df.fillna(fill_value).to_dict('list')
     return {
-        'null_counts': nc,
-        'cleaned_data': df.to_dict('list')
+        "null_counts": nc,
+        "cleaned_data": f
     }
