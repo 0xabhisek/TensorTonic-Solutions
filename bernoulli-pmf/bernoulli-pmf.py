ba51c57130ef10f7f36e0dd73e0220b699ac0d47
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-10 07:14:41 +0530

    Solve bernoulli-pmf

--- bernoulli-pmf/bernoulli-pmf.py
+++ bernoulli-pmf/bernoulli-pmf.py
@@ -8,9 +8,7 @@ def bernoulli_pmf_and_moments(x, p):
     pmf = []
     x = np.array(x, dtype = float)
     n = len(x)
-    pmf = [ p if j == 1 else (1 - p) for j in x]
-
-    pmf = np.array(pmf)
+    pmf = np.where(x==1,p,1-p)
     mu = p
     var = p*(1-p)
     return pmf,mu,var
