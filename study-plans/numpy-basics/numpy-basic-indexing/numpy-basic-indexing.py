07337c130e8d8ce66bca319b7253575c515f2916
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-13 20:34:00 +0530

    Solve numpy-basic-indexing

--- study-plans/numpy-basics/numpy-basic-indexing/numpy-basic-indexing.py
+++ study-plans/numpy-basics/numpy-basic-indexing/numpy-basic-indexing.py
@@ -4,5 +4,6 @@ def extract_subarray(arr, row_start, row_stop, col_start, col_stop):
     """
     Returns: 2D ndarray of float64
     """
-    a = np.array(arr,dtype = np.float64)
-    return a[row_start:row_stop,col_start:col_stop]
+    arr = np.array(arr, dtype = np.float64)
+
+    return arr[row_start:row_stop, col_start: col_stop]
