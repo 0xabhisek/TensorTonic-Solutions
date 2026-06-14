77ae84cb6bd6efb4f49c3517e32139b6fd3d6d0d
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-14 18:29:52 +0530

    Solve numpy-outer-sum

--- study-plans/numpy-basics/numpy-outer-sum/numpy-outer-sum.py
+++ study-plans/numpy-basics/numpy-outer-sum/numpy-outer-sum.py
@@ -2,6 +2,7 @@ import numpy as np
 
 def outer_sum(a, b):
     """Returns: np.ndarray of shape (m, n), outer sum where out[i,j] = a[i] + b[j]"""
-    x = np.array(a, dtype=np.float64)
-    y = np.array(b, dtype=np.float64)
-    return x[:, np.newaxis] + y
+    
+    a = np.array(a, dtype=np.float64)
+    b = np.array(b, dtype=np.float64)
+    return a[:, np.newaxis] + b
