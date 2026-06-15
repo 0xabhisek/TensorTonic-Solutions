11b882792675ccbea99bce6936fe61f61fc18b51
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-15 20:26:17 +0530

    Solve pandas-read-csv

--- study-plans/pandas-basics/pandas-read-csv/pandas-read-csv.py
+++ study-plans/pandas-basics/pandas-read-csv/pandas-read-csv.py
@@ -6,8 +6,7 @@ def create_dataframe(data):
     """
     df = pd.DataFrame(data)
     return {
-        'data': df.to_dict('list'),
-        'shape': list(df.shape),
-        'columns': df.columns.tolist()
+        "data": df.to_dict("list"),
+        "shape": list(df.shape),
+        "columns": df.columns.tolist()
     }
- 
