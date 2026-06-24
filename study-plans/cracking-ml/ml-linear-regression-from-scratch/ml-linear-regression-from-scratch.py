de9996410a3e0ba14a58b0ab17520d4038c7f73a
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-24 22:26:40 +0530

    Solve ml-linear-regression-from-scratch

---
+++ study-plans/cracking-ml/ml-linear-regression-from-scratch/ml-linear-regression-from-scratch.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def linear_regression(X, y, lr, epochs):
+    X = np.array(X, dtype=np.float64)
+    y = np.array(y, dtype=np.float64)
+
+    n, d = X.shape
+
+    # initialize
+    w = np.zeros(d, dtype=np.float64)
+    b = 0.0
+
+    for _ in range(epochs):
+
+        # predictions
+        y_hat = X @ w + b
+
+        # gradients
+        w_grad = (2 / n) * (X.T @ (y_hat - y))
+        b_grad = (2 / n) * np.sum(y_hat - y)
+
+        # update
+        w -= lr * w_grad
+        b -= lr * b_grad
+
+    # round and convert
+    w = [round(float(x), 4) for x in w]
+    b = round(float(b), 4)
+
+    return (w, b)
