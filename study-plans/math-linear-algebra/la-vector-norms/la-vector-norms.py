d9b7a69dcb1039acff37117c2a7bb323e27930df
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-26 17:51:24 +0530

    Solve la-vector-norms

--- study-plans/math-linear-algebra/la-vector-norms/la-vector-norms.py
+++ study-plans/math-linear-algebra/la-vector-norms/la-vector-norms.py
@@ -4,10 +4,8 @@ def vector_norms(v):
     """
     Returns: float64 array of shape (3,) containing [L1, L2, L-inf] norms.
     """
-    v = np.asarray(v, dtype = np.float64)
-    l1 = np.sum(np.abs(v))
-    l2 = (np.sum(v*v))**0.5
-    l3 = np.linalg.norm(v, ord = np.inf)
-
-    return np.array([l1,l2,l3])
-    
+    v = np.array(v)
+    v1 = np.sum(np.abs(v))
+    v2 = np.linalg.norm(v)
+    v3 = np.max(np.abs(v))
+    return np.array([v1,v2,v3])
