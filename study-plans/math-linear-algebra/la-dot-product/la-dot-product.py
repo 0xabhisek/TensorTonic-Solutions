fd4874756f4a7c8f4ab8d36a291dc80b9b1f65ef
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-26 17:34:30 +0530

    Solve la-dot-product

--- study-plans/math-linear-algebra/la-dot-product/la-dot-product.py
+++ study-plans/math-linear-algebra/la-dot-product/la-dot-product.py
@@ -5,8 +5,4 @@ def dot_product(x, y):
     Compute the dot product of two 1D arrays x and y.
     Must return a float.
     """
-    x = np.asarray(x, dtype = float)
-    y = np.asarray(y, dtype = float)
-    if x.shape != y.shape:
-        raise ValueError('arrays must have same length')
     return np.dot(x,y)
