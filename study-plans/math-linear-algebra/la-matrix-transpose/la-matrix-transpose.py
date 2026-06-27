05143d911dc357d99ccaff9e521311190dc665de
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-27 16:49:06 +0530

    Solve la-matrix-transpose

--- study-plans/math-linear-algebra/la-matrix-transpose/la-matrix-transpose.py
+++ study-plans/math-linear-algebra/la-matrix-transpose/la-matrix-transpose.py
@@ -1,8 +1,6 @@
 import numpy as np
 
 def matrix_transpose(A):
-    """
-    Returns: ndarray, the transpose of A.
-    """
-    A = np.asarray(A)
+    """Returns: ndarray, the transpose of A."""
+    A = np.asarray(A, dtype=float)
     return A.T
