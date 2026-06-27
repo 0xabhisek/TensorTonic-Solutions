0f82f50421367d7c11c1e5019478b7ad4f7ea88c
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-06-27 16:52:25 +0530

    Solve la-hadamard-product

--- study-plans/math-linear-algebra/la-hadamard-product/la-hadamard-product.py
+++ study-plans/math-linear-algebra/la-hadamard-product/la-hadamard-product.py
@@ -4,7 +4,6 @@ def hadamard_product(A, B):
     """
     Returns: ndarray, the element-wise product A * B.
     """
-    A = np.asarray(A)
-    B = np.asarray(B)
-    return A * B
-            
+    A = np.asarray(A, dtype=float)
+    B = np.asarray(B, dtype=float)
+    return A*B
