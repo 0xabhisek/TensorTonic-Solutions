1378efb08c6f4a52ccb91b6ca9c116a9b9e4d336
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-07-06 20:04:55 +0530

    Solve pandas-loc-iloc

--- study-plans/pandas-basics/pandas-loc-iloc/pandas-loc-iloc.py
+++ study-plans/pandas-basics/pandas-loc-iloc/pandas-loc-iloc.py
@@ -1,11 +1,5 @@
 import pandas as pd
 
 def iloc_selection(data, row, col):
-    """
-    Returns: list [element, row_values, col_values]
-    """
     df = pd.DataFrame(data)
-    ele = df.iloc[row,col]
-    r = df.iloc[row,:]
-    c = df.iloc[:,col]
-    return [ele,r,c]
+    return [ df.iloc[row,col], df.iloc[row,:].tolist(), df.iloc[:, col].tolist()]
