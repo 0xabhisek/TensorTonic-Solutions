d8308eaa6a4908fe812e5181afe2998e8d918c86
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-07-10 20:42:15 +0530

    Solve pytorch-tensor-reshape

--- study-plans/pytorch-basics/pytorch-tensor-reshape/pytorch-tensor-reshape.py
+++ study-plans/pytorch-basics/pytorch-tensor-reshape/pytorch-tensor-reshape.py
@@ -4,12 +4,10 @@ def reshape_tensor(x, op):
     """
     Returns: list
     """
-    x = torch.tensor(x, dtype = torch.float32)
-    if op == 'flatten':
-        return x.reshape(-1)
-
-    elif op == 'squeeze':
-        return torch.squeeze(x)
-    else:
-        return x.T
-        
+    t = torch.tensor(x, dtype=torch.float32)
+    if op == "flatten":
+        return t.flatten().tolist()
+    elif op == "squeeze":
+        return t.squeeze().tolist()
+    elif op == "transpose":
+        return t.T.tolist()
