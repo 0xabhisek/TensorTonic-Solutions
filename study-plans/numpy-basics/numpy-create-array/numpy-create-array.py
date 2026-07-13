9d35bfaefa3b627a8e48a802fc2ebe47fdd90b1f
knayanight <128348758+0xabhisek@users.noreply.github.com>
2026-07-13 21:49:58 +0530

    Solve numpy-create-array

--- study-plans/numpy-basics/numpy-create-array/numpy-create-array.py
+++ study-plans/numpy-basics/numpy-create-array/numpy-create-array.py
@@ -4,5 +4,4 @@ def create_array(data):
     """
     Returns: 2D numpy array with dtype float64
     """
-    x = np.asarray(data, dtype = np.float64)
-    return x
+    return np.array(data, dtype=np.float64)
